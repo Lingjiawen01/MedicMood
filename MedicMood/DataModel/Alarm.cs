6acbc031c15513ac0297712313eb297a37771997
Michelle Ling Jia Wen <lingjiawen2001424@gmail.com>
2024-04-12 02:24:24 +0800

    update 12/4

---
+++ MedicMood/DataModel/Alarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicMood.DataModel
+{
+    public class Alarm
+    {
+        public DateTime Time { get; set; }
+        public string Label { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- MedicMood/Views/Clockpage.xaml.cs
+++ MedicMood/Views/Clockpage.xaml.cs
@@ -1,15 +1,50 @@
+﻿using MedicMood.DataModel;
+
 namespace MedicMood.Views;
 
 public partial class Clockpage : ContentPage
 {
-	public Clockpage()
-	{
+    public List<Alarm> Alarms { get; set; }
+    private int nextAlarmNumber = 1; // 用于跟踪下一个闹钟的编号
+    public Clockpage()
+    {
         InitializeComponent();
+        LoadAlarms();
+    }
+
+    private void LoadAlarms()
+    {
+        // Load no sample alarms (replace with actual data retrieval logic)
+        Alarms = new List<Alarm>();
+
+        // Update ListView with alarms
+        alarmListView.ItemsSource = Alarms;
     }
 
     private void OnButtonClicked(object sender, EventArgs e)
     {
-        // Navigate to the AddPage when the button is clicked
-        Navigation.PushAsync(new AddPage());
+        // 创建一个新的闹钟并将其添加到列表中，标签设置为"Alarm {nextAlarmNumber}"
+        Alarm newAlarm = new Alarm { Label = $"Alarm {nextAlarmNumber}", Time = DateTime.Now.AddHours(1), IsActive = true };
+        Alarms.Add(newAlarm);
+        // Navigate to the AddPage when the button is clicked, add alarm button
+        //Navigation.PushAsync(new AddPage());
+
+        alarmListView.ItemsSource = null; // Refresh ListView
+        alarmListView.ItemsSource = Alarms;
+
+        // 增加下一个闹钟的编号
+        nextAlarmNumber++;
+    }
+    private void DeleteAlarm_Clicked(object sender, EventArgs e)
+    {
+        // Delete selected alarm from the list
+        if (alarmListView.SelectedItem != null)
+        {
+            Alarms.Remove((Alarm)alarmListView.SelectedItem);
+            alarmListView.SelectedItem = null; // Clear selection
+            alarmListView.ItemsSource = null; // Refresh ListView
+            alarmListView.ItemsSource = Alarms;
+        }
+
     }
 }
